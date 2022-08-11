e8eb009129210d89751dcd93bc3e6f9a3a4dd425
«Vitaly» <statskvo@mail.ru>
2022-08-12 00:59:44 +0300

    add folder third

---
+++ third/Program.cs
@@ -0,0 +1,6 @@
+﻿int numA = new Random().Next(1, 10); // случайное целое число из диапазона 1 2 3 4 5 6 7 8 9
+Console.WriteLine(numA);
+int numB = new Random().Next(1, 10);
+Console.WriteLine(numB);
+int result = numA + numB;
+Console.WriteLine(result);
