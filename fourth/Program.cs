68afec0e48dcf616f429ec689f033d9864cbb257
«Vitaly» <statskvo@mail.ru>
2022-08-12 01:35:14 +0300

    add folder fourth

---
+++ fourth/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("введите ваше имя, ");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша") // ToLower() - позволяет все символы строки перевести в нижний регистр
+{
+    Console.WriteLine("хороша Маша, да не наша");
+}
+else
+{
+    Console.Write("привет, ");
+    Console.WriteLine(username);
+}
