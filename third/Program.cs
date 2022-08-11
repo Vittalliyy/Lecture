d1aaba9d54011e63c81008493fd112076d168f0e
«Vitaly» <statskvo@mail.ru>
2022-08-12 01:09:38 +0300

    file changes program.cs

--- third/Program.cs
+++ third/Program.cs
@@ -1,4 +1,4 @@
-﻿int numA = new Random().Next(1, 10); // случайное целое число из диапазона 1 2 3 4 5 6 7 8 9
+﻿int numA = new Random().Next(1, 10); // случайное целое число из диапазона 1 2 3 4 5 6 7 8 9, от min до max-1
 Console.WriteLine(numA);
 int numB = new Random().Next(1, 10);
 Console.WriteLine(numB);
